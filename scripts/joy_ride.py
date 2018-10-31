9daa7043fba28ff8e2125f8d1e458c89befd5cf9
Daniel McDonald <daniel.w.mcdonald@gmail.com>
2018-10-30 23:49:21 -0400

    added obstacle avoidance override

--- scripts/joy_ride.py
+++ scripts/joy_ride.py
@@ -7,6 +7,7 @@
 #
 # Publishes to:
 #   /cmd_vel
+#   /control
 #
 # Translate joystick signals into movement commands (Joy -> Twist)
 #===================================================================
@@ -16,6 +17,7 @@ import atexit
 import geometry_msgs.msg
 import rospy
 import sensor_msgs.msg
+import std_msgs.msg
 import sys
 
 AXIS_LX = 0
@@ -23,6 +25,7 @@ AXIS_LY = 1
 AXIS_RZ = 3
 BUTTON_RB = 5
 BUTTON_LB = 4
+BUTTON_START = 7
 
 MAX_NORMAL_SPEED = 0.32
 MAX_TURBO_SPEED = 1.0
@@ -38,13 +41,22 @@ class ObstacleAvoidanceNode(object):
 
 	self.msg = None
 
-	joy_topic = rospy.get_param("~joy", "/joy")
-	rospy.loginfo('Parameter %s has value %s', rospy.resolve_name('~joy'), joy_topic)
-	cmd_topic = rospy.get_param("~cmd", "/cmd_vel_raw")
-	rospy.loginfo('Parameter %s has value %s', rospy.resolve_name('~cmd'), cmd_topic)
+	joy_topic = self.get_param("~joy", "/joy")
+	cmd_topic = self.get_param("~cmd", "/cmd_vel_raw")
+	control_topic = self.get_param("~control", "/control")
 
 	self.cmd_pub = rospy.Publisher(cmd_topic, geometry_msgs.msg.Twist, queue_size=50)
+	self.control_pub = rospy.Publisher(control_topic, std_msgs.msg.String, queue_size=10)
 	joy_sub = rospy.Subscriber(joy_topic, sensor_msgs.msg.Joy, self.on_joy)
+
+	self.current_override = False
+	self.published_override = False
+
+
+    def get_param(self, param, default):
+	value = rospy.get_param(param, default)
+	rospy.loginfo('Parameter %s has value %s', rospy.resolve_name(param), value)
+	return value
 
 
     def run(self):
@@ -52,6 +64,13 @@ class ObstacleAvoidanceNode(object):
 	while not rospy.is_shutdown():
 	    if self.msg:
 		self.cmd_pub.publish(self.msg)
+	    if self.current_override != self.published_override:
+	    	if self.current_override:
+		    self.control_pub.publish("joy_override")
+		else:
+		    self.control_pub.publish("joy_normal")
+		self.published_override = self.current_override
+	
 	    r.sleep()
 
 
@@ -74,6 +93,8 @@ class ObstacleAvoidanceNode(object):
 	msg.angular.y = 0
 	msg.angular.z = max_rotate * -joy.axes[AXIS_RZ]
 	self.msg = msg
+
+	self.current_override = joy.buttons[BUTTON_START] > 0
 
 
 if __name__ == '__main__':
