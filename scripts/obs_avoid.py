Fixed computation of obstacle vector to account for closest point on robot boundary

scripts/obs_avoid.py
@@ -56,6 +56,7 @@ def interpolate_points(points, points_per_edge):
 
 
 def find_closest_point_on_boundary(obs, boundary):
+    # TODO convert this to 3D geometry
     b = np.array([obs.dx, obs.dy])	# obstacle position
     r_index = np.argmin([np.linalg.norm(b - r) for r in boundary])
     return r_index
@@ -68,8 +69,12 @@ def normalize(v):
     return v / norm
 
 
-def obs_vec_xy(obs):
-    return normalize(np.array([obs.dx, obs.dy]))
+def obs_vec_xy(obs, r):
+    """
+    Return the vector from the closest point on the robot boundary r to the obstacle obs.
+    """
+    # TODO convert this to 3D geometry
+    return normalize(np.array([obs.dx - r[0], obs.dy - r[1]]))
 
 
 class ObstacleAvoidanceNode(object):
@@ -162,8 +167,8 @@ class ObstacleAvoidanceNode(object):
 
 		# find closest point on robot
 		r_index = find_closest_point_on_boundary(obs, self.boundary)
-		#r = self.boundary[r_index]		# closest point on robot boundary
-	    	ov = obs_vec_xy(obs)
+		r = self.boundary[r_index]		# closest point on robot boundary
+	    	ov = obs_vec_xy(obs, r)
 		p = np.dot(v, ov)  			# calculate projection on obstacle vector
 		if (p > 0):				# if a component of velocity is going toward the sensor
 		    scale = self.compute_scale(obs.range)

scripts/tc_obs_avoid.py
@@ -8,6 +8,19 @@ class TestObstacleAvoidance(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
         super(TestObstacleAvoidance, self).__init__(*args, **kwargs)
+	"""
+	  Y
+	  ^
+	  | front
+	  |
+	1 X       X
+	  |
+    left  | Robot    right
+	  |
+	0 X-------X------->X
+	  0       1
+	    back
+	"""
     	self.boundary = []
 	self.boundary.append((0, 0))  # back left
 	self.boundary.append((1, 0))  # back right
@@ -40,29 +53,74 @@ class TestObstacleAvoidance(unittest.TestCase):
 	self.assertEquals(3, obs_avoid.find_closest_point_on_boundary(self.obs, self.boundary))
 
 
+    def test_should_calculate_vector_from_boundary_to_obstacle(self):
+    	"""
+	Given the robot boundary and an obstacle position,
+	Then calculate the vector from the closest point on the boundary to the obstacle.
+	"""
+	# define obstacle position as centered in front of the robot
+	obs = self.obs
+	obs.dx = 0.5
+	obs.dy = 2
+
+	# interpolate the midpoints around the boundary
+	boundary = obs_avoid.interpolate_points(self.boundary, 1)
+
+	# find closest point on boundary
+	r_index = obs_avoid.find_closest_point_on_boundary(obs, boundary)
+
+	# get the clostest point
+	r = boundary[r_index]
+
+	# should be the midpoint on the front of the robot
+	self.assertEquals("[ 0.5  1. ]", str(r), "expected midpoint on the front of the robot, but was " + str(r))
+
+	# calculate direction from closest point to the obstacle
+	ov = obs_avoid.obs_vec_xy(obs, r)
+
+	# should point directly to front
+	self.assertEquals("[ 0.  1.]", str(ov))
+
+
     def test_calculate_velocity_adjustment(self):
     	"""
 	Given the robot boundary and a linear velocity,
 	When an obstacle is in close proximity,
 	Then the velocity should be reduced in the direction of the obstacle.
 	"""
-	# define obstacle position
+	# define obstacle position as centered in front of the robot
 	obs = self.obs
 	obs.dx = 0.5
 	obs.dy = 2
 
-	# find closest point on boundary
+	# interpolate the midpoints around the boundary
 	boundary = obs_avoid.interpolate_points(self.boundary, 1)
-	r_index = obs_avoid.find_closest_point_on_boundary(obs, boundary)
-	r = boundary[r_index]
-	self.assertEquals("[ 0.5  1. ]", str(r))
 
-	# TODO clean this into a function and share with obs_avoid.py
+	# find closest point on boundary
+	r_index = obs_avoid.find_closest_point_on_boundary(obs, boundary)
+
+	# get the closest point
+	r = boundary[r_index]
+
+	# should be the midpoint on the front of the robot
+	self.assertEquals("[ 0.5  1. ]", str(r), "expected midpoint on the front of the robot, but was " + str(r))
+
+	# calculate direction from closest point to the obstacle
+	ov = obs_avoid.obs_vec_xy(obs, r)
+
+	# calculate projection of linear velocity on obstacle direction vector
+	# TODO add tangential linear velocity derived from the angular velocity to the simple linear velocity
 	v = np.array((0, 1))  # 1 m/s velocity in Y direction
-	ov = obs_avoid.obs_vec_xy(obs)
-	obs_avoid.normalize(np.array([obs.dx, obs.dy]) - r)
-	p = np.dot(v, ov)  			# calculate projection on obstacle vector
-	self.assertTrue(np.linalg.norm(p) > 0.97)
+	p = np.dot(v, ov)
+
+	# should be in the same direction
+	self.assertTrue(abs(np.linalg.norm(p) - 1) < 1.0e-5, "expected obstacle in same direction as velocity")
+
+	# TODO finish the test with compute_scale and verification of reduced velocity in direction of obstacle
+
+	# TODO split this out into test for the projected velocity in the obstacle direction and separate test for the velocity adjustment
+
+	# TODO eliminate duplication in the test fixture between this and the previous test
 
 
 if __name__ == '__main__':